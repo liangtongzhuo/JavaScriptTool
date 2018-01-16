835979f94c20eaa0df037c5531b626780cd192f4
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2018-01-16 08:37:10 +0800

    提交完成

---
+++ 计算经两个纬度之间的距离.js
@@ -0,0 +1,11 @@
+const now = new Date();
+const hour = now.getHours();
+let hello = '';
+if (hour < 6) { hello = "凌晨好" }
+else if (hour < 9) { hello = "早上好" }
+else if (hour < 12) { hello = "上午好" }
+else if (hour < 14) { hello = "中午好" }
+else if (hour < 17) { hello = "下午好" }
+else if (hour < 19) { hello = "傍晚好" }
+else if (hour < 22) { hello = "晚上好" }
+else { hello = "夜间好" }
